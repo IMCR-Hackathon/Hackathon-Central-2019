701146db239cd998e9b52f43a8020c1d72415ff3
John Porter <jhp7e@users.noreply.github.com>
2019-06-12 15:33:01 -0400

    general report functions for num & cat

--- R/Jporter.r
+++ R/Jporter.r
@@ -1,54 +1,63 @@
-# Package ID: knb-lter-vcr.153.24 Cataloging System:https://pasta.edirepository.org.
-# Data set title: High and Low Tides of Hog Island Bay, Redbank, VA, and Oyster, VA for the Virginia Coast Reserve 2007-2018.
-# Data set creator:  John Porter -  
-# Data set creator:  David Krovetz -  
-# Data set creator:  James Spitler -  
-# Data set creator:  Thomas Williams -  
-# Data set creator:  Kathleen Overman -  
-# Data set creator:  William Nuttle -  
-# Metadata Provider:    - Virginia Coast Reserve Long-Term Ecological Research Project 
-# Contact:  John Porter -    - jhp7e@virginia.edu
-# Contact:    - Information manager - Virginia Coast Reserve Long-Term Ecological Research Project   - jporter@lternet.edu
-# Stylesheet v1.3 for metadata conversion into program: John H. Porter, Univ. Virginia, jporter@Virginia.edu 
-#
-#install package tidyverse if not already installed
-if(!require(tidyverse)){ install.packages("tidyverse") }  
-library("tidyverse") 
-infile1 <- trimws("https://pasta.lternet.edu/package/data/eml/knb-lter-vcr/153/24/e7ab56ded773a2631a2fecaeb88de3f9") 
-infile1 <-sub("^https","http",infile1)
-# This creates a tibble named: dt1 
-	dt1 <-read_delim(infile1  
-                ,delim=","   
-                ,skip=22 
-                    ,quote='"'  
-                    , col_names=c( 
-                        "STATION",   
-                        "TYPE",   
-                        "DATE",   
-                        "TIME",   
-                        "TIDE",   
-                        "WTEMP"   ), 
-                    col_types=list( 
-                        col_character(),  
-                        col_character(), 
-                        col_character(), 
-                        col_number() , 
-                        col_number() , 
-                        col_number() ), 
-                        na=c( " ",".","NA")  )
-                    
-                
-# Observed issues when reading the data. An empty list is good!
-problems(dt1) 
-# Here is the structure of the input data tibble: 
-glimpse(dt1) 
-# And some statistical summaries of the data 
-summary(dt1) 
-# Get more details on character variables
-                     
-summary(as.factor(dt1$STATION)) 
-summary(as.factor(dt1$TYPE))
+make_vector_of_numeric_variable_names<-function(df){
+  numeric_var_name_vector<-c("")
+  attach(df,warn.conflicts = F)
+  for(varname in names(df)) {
+    if(is.numeric(get(varname))){
+      numeric_var_name_vector<-append(numeric_var_name_vector, varname, after = length(numeric_var_name_vector))
+    }
+  }
+  detach(df)
+  numeric_var_name_vector<-tail(numeric_var_name_vector,length(numeric_var_name_vector)-1)
+  return(numeric_var_name_vector)
+}
 
 
+make_vector_of_categorical_variable_names<-function(df){
+  cat_var_name_vector<-c("")
+  attach(df,warn.conflicts = F)
+  for(varname in names(df)) {
+    var<-get(varname)
+    if(is.factor(var) | is_character(var)){
+      cat_var_name_vector<-append(cat_var_name_vector, varname, after = length(cat_var_name_vector))
+    }
+  }
+  detach(df)
+  cat_var_name_vector<-tail(cat_var_name_vector,length(cat_var_name_vector)-1)
+  return(cat_var_name_vector)
+}
 
+make_numeric_variable_summary_df<-function(var){
+  var_median<-median(var,na.rm=T)
+  var_max<-max(var,na.rm=T)
+  var_min<-min(var,na.rm=T)
+  num_valid<-sum(!is.na(var))
+  num_missing<-sum(is.na(var))
+  df<-data.frame(var_median,var_max,var_min,num_valid,num_missing)
+  return(df)
+}
 
+make_cat_variable_summary_df<-function(var){
+  var_type<-class(var)
+  num_levels<-length(levels(as.factor(var)))
+  num_valid<-sum(!is.na(var))
+  num_missing<-sum(is.na(var))
+  df<-data.frame(var_type,num_levels,num_valid,num_missing)
+  return(df)
+}
+
+make_numeric_histogram<-function(df,var){
+  library(ggplot2)
+  varname=deparse(substitute(var))
+  x<-ggplot(df,aes(x=var))+ xlab(varname)+
+  geom_histogram(aes(y=..density..), colour="black", fill="white")+
+    geom_density(alpha=.2, fill="#FF6666") 
+  return(x)
+}
+
+make_categorical_histogram<-function(df,var){
+  library(ggplot2)
+  varname=deparse(substitute(var))
+  x<-ggplot(df,aes(x=var))+ xlab(varname)+
+    geom_bar() 
+  return(x)
+}
