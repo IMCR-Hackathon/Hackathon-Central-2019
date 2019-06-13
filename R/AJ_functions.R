7087fc8464286a72418035566ed3274f93132936
alesiahallmark <alesia.hallmark@gmail.com>
2019-06-13 12:16:03 -0600

    # adding functions for Dates, in addition to the POSIX functions

--- R/AJ_functions.R
+++ R/AJ_functions.R
@@ -1,7 +1,7 @@
 ### This file contains a list of functions that Alesia wrote
 ### These functions are meant to summarize time (POSIX, Date) objects in a data.frame
 
-## make_vector_of_POSIX_variable_names
+## make_vector_of_Date_variable_names
 # this function makes a list of POSIX class columns in a data.frame
 make_vector_of_POSIX_variable_names<-function(df){
   POSIX_var_name_vector<-c("")
@@ -15,6 +15,22 @@ make_vector_of_POSIX_variable_names<-function(df){
   detach(df)
   POSIX_var_name_vector<-tail(POSIX_var_name_vector,length(POSIX_var_name_vector)-1)
   return(POSIX_var_name_vector)
+}
+
+## make_vector_of_POSIX_variable_names
+# this function makes a list of POSIX class columns in a data.frame
+make_vector_of_Date_variable_names<-function(df){
+  Date_var_name_vector<-c("")
+  attach(df,warn.conflicts = F)
+  for(varname in names(df)) {
+    var<-get(varname)
+    if(is.Date(var)){
+      Date_var_name_vector<-append(Date_var_name_vector, varname, after = length(Date_var_name_vector))
+    }
+  }
+  detach(df)
+  Date_var_name_vector<-tail(Date_var_name_vector,length(Date_var_name_vector)-1)
+  return(Date_var_name_vector)
 }
 
 ## make_time_variable_summary_df
@@ -34,18 +50,48 @@ make_time_variable_summary_df<-function(var){
 
 ## freqclocks
 # this function creates ggplot clocks of the frequency of data occurring at different time frequencies, if POSIX type data
-freqclocks <- function(df,var,varname) {
+freqclocks_forDates <- function(df,var,varname) {
   na.df <- as.data.frame(!is.na(df))
   na.df[,varname] <- df[,varname]
   long.df <- gather(na.df, key = column.name, value = count, -varname)
   
   freq.timeline <- ggplot(long.df, aes(x = long.df[,1], group = column.name)) + 
     theme_bw() +
-    geom_freqpoly(aes(colour = column.name)) + 
-    ylab("Data Frequency") + xlab("Time") +
-    scale_x_datetime(labels=date_format("%b-%Y")) +
+    geom_freqpoly(aes(colour = column.name), bins = 100) + 
+    ylab("Data Frequency") + xlab("Time")  +
     guides(colour = guide_legend("Column Names")) +
-    theme(legend.position = "top")
+    theme(legend.position = "top") + 
+    scale_x_date(labels=date_format("%b-%Y"))
+  
+  # plot how much data occurs in each month of the year
+  month.clock <- ggplot(long.df, aes(x = months(long.df[,1], T), group = column.name)) + 
+    theme_bw() +
+    geom_histogram(stat = "count", aes(fill = column.name)) + 
+    coord_polar() + xlim(month.abb) +
+    ylab("Data Coverage") + xlab("Month of Year") +
+    guides(fill = guide_legend("Column Names")) +
+    theme(legend.position = "none")
+  
+  Date.plots <- plot_grid(freq.timeline, month.clock,
+                           ncol=1, align = "v", rel_widths = c(3,1))
+  return(Date.plots)
+}
+
+
+## freqclocks
+# this function creates ggplot clocks of the frequency of data occurring at different time frequencies, if POSIX type data
+freqclocks_forPOSIX <- function(df,var,varname) {
+  na.df <- as.data.frame(!is.na(df))
+  na.df[,varname] <- df[,varname]
+  long.df <- gather(na.df, key = column.name, value = count, -varname)
+  
+  freq.timeline <- ggplot(long.df, aes(x = long.df[,1], group = column.name)) + 
+    theme_bw() +
+    geom_freqpoly(aes(colour = column.name), bins = 100) + 
+    ylab("Data Frequency") + xlab("Time")  +
+    guides(colour = guide_legend("Column Names")) +
+    theme(legend.position = "top") + 
+    scale_x_datetime(labels=date_format("%b-%Y")))
   
   # plot how much data occus in each hour of the day
   hour.clock <- ggplot(long.df, aes(x = hour(long.df[,1]), group = column.name)) + 
@@ -67,13 +113,15 @@ freqclocks <- function(df,var,varname) {
   
   both.clocks <- plot_grid(month.clock, hour.clock,
                            nrow=1, align = "h", rel_widths = c(1,1))
-  three.plots <- plot_grid(freq.timeline, both.clocks,
+  POSIX.plots <- plot_grid(freq.timeline, both.clocks,
                            ncol=1, align = "v", rel_widths = c(1,1))
   return(three.plots)
 }
 
-df <- full.data.pkg
-varname <- "timestamp"
-var <- full.data.pkg[,"timestamp"]
+
+df <- dt1
+varname <- "DATE"
+var <- df[,"DATE"]
 
 
+dt1$DATE <- as.Date(dt1$DATE, "%d%b%Y")
